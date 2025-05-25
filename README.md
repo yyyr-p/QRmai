e95f45ebe88211b2c76c6f6f17c5c010bff91742
Soda <soda_code@outlook.com>
2025-05-25 20:58:25 +0800

    test version

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # QRmai
-Dynamically generate login QR code for the game maimaiDX
+动态生成舞萌DX登录二维码，灵感来源于[MaimaiHelper](https://github.com/SomeUtils/MaimaiHelper)
+
+## 为什么使用QRmai而不是使用MaimaiHelper
+1. QRmai无需配置运行环境
+2. QRmai具有更多的自定义选项
+3. QRmai兼容MaimaiHelper APP
+
+## 部署教程
+运行Release下的`QRmai.exe`，然后访问127.0.0.1:5000即可获取二维码
---
+++ main.py
@@ -0,0 +1,53 @@
+from flask import Flask, Response, request
+import io
+import time
+
+import pyautogui
+import pygetwindow as gw
+import qrcode
+from PIL import Image
+from mss import mss
+from pyzbar.pyzbar import decode
+
+app = Flask(__name__)
+
+@app.route('/qrmai')
+def qrmai():
+    if request.args.get('token') != config['token']:
+        return "error"
+    window = gw.getWindowsWithTitle("微信")[0]
+
+    window.activate()
+
+    def move_click(x, y):
+        pyautogui.moveTo(x, y)
+        pyautogui.click()
+
+
+    move_click(config["p1"][0], config["p1"][1])
+
+    time.sleep(2)
+    move_click(config["p2"][0], config["p2"][1])
+
+    time.sleep(2)
+    with mss() as sct:
+        # 截取整个屏幕
+        screenshot = sct.grab(sct.monitors[1])  # monitors[1] 表示第一个显示器
+        image = Image.frombytes("RGB", screenshot.size, screenshot.rgb)
+
+    # 解码二维码
+    decoded_objects = decode(image)
+
+    qr_img = qrcode.make(decoded_objects[0].data.decode("utf-8"))
+
+    img_io = io.BytesIO()
+    qr_img.save(img_io, format='PNG')
+    img_io.seek(0)
+
+    return Response(img_io, mimetype='image/png')
+
+if __name__ == '__main__':
+    import json
+    with open('config.json', 'r', encoding='utf-8') as f:
+        config = json.load(f)
+    app.run(host='127.0.0.1', port=5000)
